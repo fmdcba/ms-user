8a03ed8524a2aadd43595217eaa3b814a1ba60dd
fmdcba <facudiaz1738@hotmail.com>
2025-01-31 05:18:07 -0300

    config rabbit prod

---
+++ src/main/java/com/mindhub/ms_user/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.mindhub.ms_user.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    public static final String EXCHANGE_NAME = "app.exchange";
+    public static final String USER_CREATED_ROUTING_KEY = "user.created";
+
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(jsonMessageConverter);
+        return template;
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory) {
+        return rabbitTemplate(connectionFactory, jsonMessageConverter());
+    }
+}
+
